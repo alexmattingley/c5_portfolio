e56ab35e505228951d5184b18f9ef0c5e2f88d3e
Alex Mattingley <alexmattingley@gmail.com>
2015-08-03 08:53:15 -0700

    boxes up next for js

--- week3/Bootstrap-Grid/main.js
+++ week3/Bootstrap-Grid/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 
+	function hero_text_fadeIn() {
+		$('.hero-text h1').fadeIn(3000);
 
-	$('.hero-text h1').fadeIn(3000);
+		function second_fade() {
+			$('.hero-text h4').fadeIn(2000);
+			$('.hero-text a.glyphicon-menu-down.text-center').fadeIn(2000);
+		}
 
-	function second_fade() {
-		$('.hero-text h4').fadeIn(3000);
-		$('.hero-text a.glyphicon-menu-down.text-center').fadeIn(3000);
+		setTimeout(second_fade, 2000);
 	}
 
-	setTimeout(second_fade, 2000);
+	hero_text_fadeIn();
 });
