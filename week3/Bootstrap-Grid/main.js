57e682598c215a48b6dfb297c4514c2922299532
Alex Mattingley <alexmattingley@gmail.com>
2015-08-03 08:07:52 -0700

    appending on load, gonna see how that fadeIn method works.

--- week3/Bootstrap-Grid/main.js
+++ week3/Bootstrap-Grid/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 
+	var hero_text = $('.hero-text');
 	var hero_h1 = $('<h1>').text('hello.');
-	$('.hero-text').append(hero_h1);
+	var hero_h4 = $('<h4>').text('My name is Alex Mattingley and I develop websites and applications.');
+	var down_arrow = $('<a>',{
+		class: "glyphicon glyphicon-menu-down text-center",
+		href: "#work"
+	});
+	
+
+	hero_text.append(hero_h1);
+	hero_text.append(hero_h4);
+	hero_text.append(down_arrow);
 
 });
