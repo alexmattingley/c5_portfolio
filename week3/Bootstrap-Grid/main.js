0907965d0305ccf61c09516411ccc780d0b94c09
Alex Mattingley <alexmattingley@gmail.com>
2015-08-07 09:41:29 -0700

    more clean up and comments

--- week3/Bootstrap-Grid/main.js
+++ week3/Bootstrap-Grid/main.js
@@ -1,10 +1,12 @@
+// Basic Function for the hero banner h1 fadeIn, called in the document ready
+function hero_text_fadeIn() {
+	$('.hero-text h1').fadeIn(3000);	
+}
+
+//basic function for the rest of the text and the down arrow fadeIn, called in the document ready
 function second_fade() {
 	$('.hero-text h4').fadeIn(2000);
 	$('.hero-text a.glyphicon-menu-down.text-center').fadeIn(2000);
-}
-
-function hero_text_fadeIn() {
-	$('.hero-text h1').fadeIn(3000);	
 }
 
 $(document).ready(function(){
