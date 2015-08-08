a81f4b682a4c8626f5e6ca92205bef280f682e80
Alex Mattingley <alexmattingley@gmail.com>
2015-08-08 10:13:35 -0700

    implements vert scroll. I also add a bunch of comments to the code because things are getting more complex

--- week3/Bootstrap-Grid/main.js
+++ week3/Bootstrap-Grid/main.js
@@ -1,3 +1,9 @@
+/****************
+Globals
+*****************/
+
+var project_load_click = 0;
+
 // Basic Function for the hero banner h1 fadeIn, called in the document ready
 function hero_text_fadeIn() {
 	$('.hero-text h1').fadeIn(3000);	
@@ -9,16 +15,31 @@ function hero_second_fade() {
 	$('.hero-text a.glyphicon-menu-down.text-center').fadeIn(2000);
 }
 
+//basic function for fadein of text in the individual projects
 $('.indiv-project').on('mouseenter', function(){
 	$(this).find('p').fadeIn(2000);
 });
 
+//This sets the project rows the correct height in order that the vertical carousel
+//below will work properly. 
 var project_row_height = $('.project-row').height();
 $('.work-block ul.list-unstyled').css('height', project_row_height);
 
+/*************
+This function is the meat and potatoes of the vert scroll. On the click of the 
+more-projects button, it animates the rows to scroll up or down depending on
+if the user is currently looking at the first or the second row of projects.
+*************/
 $('.more-projects').on('click', function(){
-	$('#proj-row-1').animate({top: '-100%'}, 2000);
-	$('#proj-row-2').animate({top: '-100%'}, 2000);
+	project_load_click = ++project_load_click;
+	if (project_load_click < 2) {
+		$('#proj-row-1').animate({top: '-=100%'}, 2000);
+		$('#proj-row-2').animate({top: '-=100%'}, 2000);
+	}else {
+		$('#proj-row-1').animate({top: '0%'}, 2000);
+		$('#proj-row-2').animate({top: '0%'}, 2000);
+	}
+	console.log(project_load_click);
 });
 
 
