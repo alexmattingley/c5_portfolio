b6fec2edc8d472beca19e96b08f719e04b3e7fe7
Alex Mattingley <alexmattingley@gmail.com>
2015-08-10 09:42:24 -0700

    making my files dynamic. Boy I am tired of reproducing content

---
+++ footer.php
@@ -0,0 +1,29 @@
+<footer class="clearfix">
+			<ul class="list-unstyled col-xs-12">
+				<li class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-2 col-lg-offset-2">
+					<h4>Contact</h4>
+					<a class="p_Num" href="tel:949.280.6557"></a>
+					<a class="email_add" href="mailto:alexmattingley@gmail.com"></a>
+				</li>
+				<li class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-2 col-lg-offset-1">
+					<h4>Connect</h4>
+					<p>
+						<i class="fa fa-github fa-2x"></i>
+						<i class="fa fa-facebook-square fa-2x"></i>
+						<i class="fa fa-linkedin-square fa-2x"></i>
+					</p>
+				</li>
+				<li class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-2 col-lg-offset-1">
+					<h4>Location</h4>
+					<address>
+					  1501 Tahiti Ave<br>
+					  Laguna Beach, CA 92651<br>
+					</address>
+				</li>
+			</ul>
+		</footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Alex Mattingley | Web Development</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">	
+	</head>
+	<body>
+		<div class="hero-banner">
+			<nav class="navbar">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+						        <span class="glyphicon glyphicon-menu-hamburger"></span>
+						</button>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				      <ul class="nav navbar-nav">
+				        <li><a href="index.html">Home</a></li>
+				        <li><a href="about.html">About</a></li>
+				        <li><a href="#work">Work</a></li>
+				        <li><a href="#contact">Contact</a></li>
+				    </div>
+				</div>
+			</nav>
