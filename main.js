182ea53196c20477828c0e89283f236d0e1ed48c
Alex Mattingley <alexmattingley@gmail.com>
2015-08-31 07:26:22 -0700

    removed fadeIn, gonna work on typing function

--- main.js
+++ main.js
@@ -13,17 +13,6 @@ var phe_class = $('.p_Num');
 var phe_icon = '<span class="glyphicon glyphicon-phone-alt"></span>'
 var pNum = ' (949) 280-6557';
 
-// Basic Function for the hero banner h1 fadeIn, called in the document ready
-function hero_text_fadeIn() {
-	$('.hero-text h1').fadeIn(3000);	
-}
-
-//basic function for the rest of the text and the down arrow fadeIn, called in the document ready
-function hero_second_fade() {
-	$('.hero-text h4').fadeIn(2000);
-	$('.hero-text a.glyphicon-menu-down.text-center').fadeIn(2000);
-}
-
 //basic function for fadein of text in the individual projects
 $('.indiv-project').on('mouseenter', function(){
 	$(this).find('p').fadeIn(2000);
@@ -64,7 +53,6 @@ function add_contact_info() {
 
 $(document).ready(function(){
 	setTimeout(hero_second_fade, 2000);
-	hero_text_fadeIn();
 
 	//adds email addresses throughout
 	add_contact_info();
