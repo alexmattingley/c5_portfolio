e69d68a9db5023fa663cf3df9e31a5908d0ff34c
Alex Mattingley <alexmattingley@gmail.com>
2015-09-05 12:11:16 -0700

    basic changes

--- inc/footer.php
+++ inc/footer.php
@@ -1,16 +1,14 @@
 <footer class="clearfix">
 			<ul class="list-unstyled col-xs-12">
-				<li class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-3 col-lg-offset-1">
+				<li class="contact col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-3 col-lg-offset-1">
 					<h4>Contact</h4>
 					<a class="p_Num" href="tel:949.280.6557"></a>
 					<a class="email_add" href="mailto:alexmattingley@gmail.com"></a>
 				</li>
 				<li class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-3 col-lg-offset-1">
 					<h4>Connect</h4>
-					<p>
-						<i class="fa fa-github fa-2x"></i>
-						<i class="fa fa-linkedin-square fa-2x"></i>
-					</p>
+					<a href="https://github.com/alexmattingley"><i class="fa fa-github fa-2x"></i></a>
+					<a href="https://www.linkedin.com/pub/alex-mattingley/22/27a/8b6"></a><i class="fa fa-linkedin-square fa-2x"></i>
 				</li>
 				<li class="col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-0 col-lg-3 col-lg-offset-1">
 					<h4>Location</h4>
