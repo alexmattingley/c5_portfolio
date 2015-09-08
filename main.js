833257fd8d01c528196cb65b86a4ccdd48b283c7
Alex Mattingley <alexmattingley@gmail.com>
2015-09-07 17:21:26 -0700

    now including params.

--- main.js
+++ main.js
@@ -7,20 +7,18 @@ var project_load_click = 0;
 var h1_string = "hello.";
 var h4_string = "My name is Alex Mattingley and I develop websites and applications.";
 var current_string_index = 0;
-var min_interval = 100;
-var max_interval = 200;
-var interval_delta = max_interval - min_interval;
+
 var $hero_h1 =  $('.hero-text h1');
 var $hero_h4 = $('.hero-text h4');
 
-function type_effect(target_dom, output_string){
-
+function type_effect(target_dom, output_string, min_interval, max_interval){
+	var interval_delta = max_interval - min_interval;
 	if(current_string_index <  output_string.length){
 		target_dom.text(target_dom.text()+output_string[current_string_index++]);
 		var random_time = Math.floor(Math.random()*interval_delta+ min_interval);
 		console.log(random_time);
 		setTimeout(function(){
-			type_effect(target_dom, output_string)
+			type_effect(target_dom, output_string, max_interval, min_interval)
 		}, random_time);
 	}else{
 		current_string_index = 0;
@@ -122,9 +120,9 @@ function add_contact_info() {
 
 $(document).ready(function(){
 
-	type_effect($hero_h1, h1_string);
+	type_effect($hero_h1, h1_string, 200, 300);
 	setTimeout(function(){
-		type_effect($hero_h4, h4_string);
+		type_effect($hero_h4, h4_string, 50, 150);
 	},2000);
 	//adds email addresses throughout
 	set_work_block_height();
